9a658cb914081a7d0c3b44eeb09a6ec7d6e076bd
Swarup Hait <67947137+swaruphait3@users.noreply.github.com>
2022-11-10 18:00:31 +0530

    Delete SupportSolutions directory

--- SupportSolutions/src/main/java/com/vintegrate/support/repository/SolutionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vintegrate.support.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vintegrate.support.entity.Issues;
-
-public interface SolutionRepository extends JpaRepository<Issues, Integer>{
-
-}
